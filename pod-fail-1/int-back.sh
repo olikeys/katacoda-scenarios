c20491e1bf82d217db1404e621950425760307f5
Oli Keys <oli.keys@condenastint.com>
2020-01-24 15:46:26 +0000

    getting background script working

--- pod-fail-1/int-back.sh
+++ pod-fail-1/int-back.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-while ! grep "Completed" katacoda-finished;do sleep 10;done
+# while ! grep "Completed" katacoda-finished;do sleep 10;done
 
-kubectl apply -f /root/assets/web-svr.yaml
+# kubectl apply -f /root/assets/web-svr.yaml
 
 echo "done" >> /root/katacoda-background-finished
