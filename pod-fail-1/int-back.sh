cfcf7489f3879de57ac1a8db7071fa7e7682f2bc
Oli Keys <oli.keys@condenastint.com>
2020-01-24 14:54:48 +0000

    path wrong

--- pod-fail-1/int-back.sh
+++ pod-fail-1/int-back.sh
@@ -4,4 +4,4 @@ while ! grep "Completed" katacoda-finished;do sleep 10;done
 
 kubectl apply -f /root/assets/web-svr.yaml
 
-echo "done" >> root/katacoda-background-finished
+echo "done" >> /root/katacoda-background-finished
