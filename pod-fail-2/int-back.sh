68c62cd551ff7e7a86854037fcb8f8a2dbc5c6b9
Oli Keys <oli.keys@condenastint.com>
2020-02-04 18:07:07 +0000

    rejig ordering

--- pod-fail-2/int-back.sh
+++ pod-fail-2/int-back.sh
@@ -2,6 +2,6 @@
 
 while ! grep "Completed" katacoda-finished;do sleep 10;done
 
-kubectl apply -f /root/assets/web-svr.yaml
-
 echo "done" >> /root/katacoda-background-finished
+
+kubectl apply -f /root/assets/web-svr.yaml
