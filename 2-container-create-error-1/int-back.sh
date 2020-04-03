f3adaa6d0a801ab66a7f2448b8dbc7bf55cb1640
Oli Keys <olikeys@gmail.com>
2020-04-03 22:55:48 +0100

    change scenario2 to use non-running minikube env

--- 2-container-create-error-1/int-back.sh
+++ 2-container-create-error-1/int-back.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
-while ! grep "Completed" katacoda-finished;do sleep 10;done
-
-echo "done" >> /root/katacoda-background-finished
+while ! grep "kube-start" katacoda-foreground-finished;do sleep 10;done
 
 kubectl apply -f /root/assets/web-svr.yaml
+
+#echo "done" >> /root/katacoda-background-finished
+
--- 2-container-create-error-1/int-fore.sh
+++ 2-container-create-error-1/int-fore.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-sleep 1; ./assets/configure-env.sh
+minikube start && echo "kube-started" >> /root/katacoda-foreground-finished
 
