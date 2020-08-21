4c21ada5ef1aa702a1a3ec9714cde80eacf6c649
Oli Keys <oli.keys@condenast.com>
2020-08-21 13:36:31 +0100

    move back to background task

--- scenario-3/int-back.sh
+++ scenario-3/int-back.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
+while ! grep "kube-start" kube-started;do sleep 10;done
+
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: ResourceQuota
+metadata:
+  name: compute-resources
+  namespace: default
+spec:
+  hard:
+    limits.cpu: "25m"
+    limits.memory: 10Mi
+    requests.cpu: "25m"
+    requests.memory: 10Mi
+EOF
+clear
+kubectl apply -f web-svr.yaml && echo "deploy-complete" >> /root/katacoda-foreground-finished
+
 while ! grep "deploy-complete" katacoda-foreground-finished;do sleep 10;done
+
+echo "Environment setup is complete"
--- scenario-3/int-fore.sh
+++ scenario-3/int-fore.sh
@@ -1,20 +1,2 @@
 #! /bin/bash
 minikube start && echo "kube-started" >> /root/kube-started
-
-while ! grep "kube-start" kube-started;do sleep 10;done
-
-cat <<EOF | kubectl apply -f -
-apiVersion: v1
-kind: ResourceQuota
-metadata:
-  name: compute-resources
-  namespace: default
-spec:
-  hard:
-    limits.cpu: "25m"
-    limits.memory: 10Mi
-    requests.cpu: "25m"
-    requests.memory: 10Mi
-EOF
-clear
-kubectl apply -f web-svr.yaml && echo "deploy-complete" >> /root/katacoda-foreground-finished
