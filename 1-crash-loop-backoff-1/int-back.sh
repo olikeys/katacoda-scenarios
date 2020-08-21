4c1715ba5916d0bb4a9f2f8e5bad1ce2a0180b99
Oli Keys <oli.keys@condenast.com>
2020-08-21 13:08:07 +0100

    more cleanup

--- 1-crash-loop-backoff-1/int-back.sh
+++ 1-crash-loop-backoff-1/int-back.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 while ! grep "kube-start" katacoda-foreground-finished;do sleep 10;done
 
+kubectl apply -f /root/assets/quota.yaml
 kubectl apply -f /root/assets/web-svr.yaml
 
 #echo "done" >> /root/katacoda-background-finished
