1322e4e271c225bee91a90f2543997afb63df111
Oli Keys <oli.keys@condenast.com>
2020-08-21 13:13:08 +0100

    int-back

--- scenario-3/int-back.sh
+++ scenario-3/int-back.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 while ! grep "kube-start" katacoda-foreground-finished;do sleep 10;done
 
-kubectl apply -f quota.yaml
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: ResourceQuota
+metadata:
+  name: compute-resources
+  namespace: default
+spec:
+  hard:
+    limits.cpu: "25m"
+    limits.memory: 10Mi
+    requests.cpu: "25m"
+    requests.memory: 10Mi
+EOF
 kubectl apply -f /root/assets/web-svr.yaml
 
 #echo "done" >> /root/katacoda-background-finished
