9c8f75ec807bb770c7bea7f37523d77306e85a9e
Oli Keys <oli.keys@snyk.io>
2021-10-08 17:21:44 +0100

    update backend init

--- k8s-scenarios/4-pod-pending/int-back.sh
+++ k8s-scenarios/4-pod-pending/int-back.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 while ! grep "kube-start" katacoda-foreground-finished;do sleep 10;done
 rm /etc/kubernetes/manifests/kube-scheduler.yaml 
-kubectl delete kube-scheduler-minikube -n kube-system && echo \
-    "scheduler-dead" >> /root/katacoda-foreground-finished
+kubectl delete pod kube-scheduler-minikube -n kube-system \
+  &&echo "scheduler-dead" >> /root/katacoda-foreground-finished
 
 while ! grep "scheduler-dead" katacoda-foreground-finished;do sleep 10;done
 kubectl apply -f /root/assets/web-svr.yaml
